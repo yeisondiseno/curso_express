114489eeca12da2ecb776fd057f8277830cc5c64
yeisondiseno <yeisondiseno@gmail.com>
2025-07-17 11:06:03 -0500

    Refactor server setup: remove unused variable and add 404 error handling

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const { PORT } = require("./config/env");
 
 // Configurations
 const app = express();
-const port = PORT;
+app.disable("x-powered-by");
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.use((req, res, next) => {
+  res.status(404).send("<h1>404 Not Found</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
 });
