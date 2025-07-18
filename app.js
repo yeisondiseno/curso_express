0f1cd78046ccf7f51fd82e748107a29e87740988
yeisondiseno <yeisondiseno@gmail.com>
2025-07-18 09:29:59 -0500

    Update app.js to include MVC note and fix standard version in package.json

--- app.js
+++ app.js
@@ -5,3 +5,5 @@ import { env } from "./src/config/index.js";
 app.listen(env.PORT, () => {
   console.log(`Example app listening on port ${env.PORT}`);
 });
+
+// !NOTE: MVC https://youtu.be/ev3Yxva4wI4?si=qu3c7LRGv2WblAT6&t=3127
